f613308af1102e32dd5788de37e4333779ec231f
kwansu <kwansu91@naver.com>
2022-04-09 04:57:49 +0900

    sks

--- environment/base_environment.py
+++ environment/base_environment.py
@@ -47,7 +47,9 @@ class BaseEnvironment(ABC):
         self.selected_back_color = selected_back_color
 
         sprite_path = os.path.dirname(os.path.abspath(__file__)) + "\\sprite\\*.png"
+        print(sprite_path)
         img_dict = {splitext(basename(x))[0]: x for x in glob(sprite_path)}
+        print(img_dict)
         self._setup_render(key_queue, hotkey_funcs, img_dict)
         self._redraw()
 
